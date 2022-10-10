d991d80c78484f480c4b12f4bfe62cbd4f5ff113
Ookiineko <chiisaineko@protonmail.com>
2022-10-11 00:39:16 +0800

    update
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- src/rk05.cbl
+++ src/rk05.cbl
@@ -16,20 +16,20 @@ include "include/traps"
 
 
 macro vec3i _get_disk_pos() {
-    return vec3i(8, 24, 8);
+    return vec3i(-50, 124, -50);
 }
 
 macro vec3i _get_disk_size() {
     return vec3i(55, 128, 55);
 }
 
-macro int _disk_read(Entity armstand, int addr) {
+int _disk_read(Entity armstand, int addr) {
     vec3i disk_pos = _get_disk_pos();
     vec3i disk_size = _get_disk_size();
     return chest_flash_read(armstand, disk_pos, disk_size, addr);
 }
 
-macro void _disk_write(Entity armstand, int addr, int val) {
+void _disk_write(Entity armstand, int addr, int val) {
     vec3i disk_pos = _get_disk_pos();
     vec3i disk_size = _get_disk_size();
     chest_flash_write(armstand, disk_pos, disk_size, addr, val);
